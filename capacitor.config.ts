e4adab38dbd2789e1012ced8298f6394343b9d76
Nayely Ayol <nayely.ayol@epn.edu.ec>
2026-05-08 03:21:14 -0500

    implementacion de icono y splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,10 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'login-supabase',
-  webDir: 'www'
+  appName: 'PracticaMovil',
+  webDir: 'www',
+  bundledWebRuntime: false
 };
 
 export default config;
